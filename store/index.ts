8024a7452e862e4a4d9e2bf1f6f91643be1dc6cf
Kelvyn Krug <kelvynkrug@gmail.com>
2022-06-07 10:11:03 -0300

    final boilerplate

--- store/index.ts
+++ store/index.ts
@@ -1,8 +1,8 @@
 import { Store } from 'vuex';
-import { initializeStores } from '@/utils/store-accessor';
+import { initializeStores } from '@/utils/store-util';
 
 const initializer = (store: Store<any>) => initializeStores(store);
 
 export const plugins = [initializer];
 export const strict = false;
-export * from '@/utils/store-accessor';
+export * from '@/utils/store-util';
